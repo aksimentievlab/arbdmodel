88a338b3df7e6570e80bb7e0d34143cf516e4d90
pinyili2 <pinyili2@illinois.edu>
2025-06-20 20:38:57 -0500

    fix polymer bug and version bug

--- arbdmodel/version.py
+++ arbdmodel/version.py
@@ -42,6 +42,17 @@ _version_file = Path(__file__).parent / "RELEASE-VERSION"
 
 
 def check_git_repository():
+    """
+    Checks if the current working directory is part of a git repository
+    that has 'arbdmodel.git' as one of its remotes.
+    
+    Uses the 'git remote -v' command to get a list of remotes and 
+    checks if 'arbdmodel.git' is in the output.
+    
+    Returns:
+        bool: True if 'arbdmodel.git' is found in the remotes, False otherwise
+              or if an error occurs while running the git command.
+    """
     try:
         remotes = subprocess.check_output(['git', 'remote', '-v'], stderr=subprocess.STDOUT)
         return b'arbdmodel.git' in remotes
@@ -49,6 +60,27 @@ def check_git_repository():
         return False   
 
 def call_git_describe(abbrev):
+    """
+    Call git describe command to get the current tag and commit information.
+    
+    This function executes the 'git describe --tags --abbrev=N' command to retrieve
+    the tag and abbreviated commit hash of the current git repository.
+    
+    Parameters
+    ----------
+    abbrev : int
+        The length of the abbreviated commit hash to return.
+        
+    Returns
+    -------
+    str or None
+        The output of git describe command (tag-commits-hash) as a string if successful,
+        or None if the command fails (e.g., not in a git repository).
+    
+    Note
+    -----
+    Requires git to be installed and the code to be within a git repository.
+    """
     try:
         p = Popen(['git', 'describe', '--tags', '--abbrev=%d' % abbrev],
                   stdout=PIPE, stderr=PIPE)
@@ -61,6 +93,17 @@ def call_git_describe(abbrev):
 
 
 def is_dirty():
+    """
+    Check if the Git repository has uncommitted changes.
+
+    This function determines if the current Git repository is "dirty", meaning it has
+    uncommitted changes to tracked files. It runs the Git command 'diff-index --name-only HEAD'
+    which lists files that differ from the current HEAD commit.
+
+    Returns:
+        bool: True if there are uncommitted changes (repository is dirty), 
+              False if the repository is clean or if an error occurs when running the Git command.
+    """
     try:
         p = Popen(["git", "diff-index", "--name-only", "HEAD"],
                   stdout=PIPE, stderr=PIPE)
@@ -139,7 +182,54 @@ def read_version_file(filename):
     with open(filename) as fh:
         version = fh.readlines()[0].strip()
     return version
-        
+
+class Citation():
+    """
+    A class to represent a citation in academic publications.
+
+    This class provides functionality to create and display a citation with various
+    bibliographic information such as author, title, journal, etc.
+
+    Attributes:
+        author (str): The author(s) of the publication.
+        title (str): The title of the publication.
+        journal (str): The journal in which the publication appeared.
+        volume (str): The volume number of the publication.
+        number (str): The issue number of the publication.
+        pages (str): The page range of the publication.
+        year (str): The year of publication.
+        doi (str): The Digital Object Identifier or URL of the publication.
+
+    Methods:
+        display(): Returns a formatted string representation of the citation.
+    """
+    def __init__(self, author=None, title=None, journal=None, volume=None, number=None, pages=None, year=None, doi=None):
+        self.author = author
+        self.title = title
+        self.journal = journal
+        self.volume = volume
+        self.number = number
+        self.pages = pages
+        self.year = year
+        self.doi = doi
+
+    def display(self):
+        msg = ''
+        if self.author: msg = f'{self.author}. '
+        if self.title: msg = f'{msg}{self.title}. '
+        if self.journal: msg = f'{msg}{self.journal} '
+        if self.volume:
+            msg = f'{msg} {self.volume}{"," if any((self.number, self.pages, self.year)) else "."} '
+            if self.number:
+                msg = f'{msg}({self.number}){"" if any((self.pages,self.year)) else "."} '
+            if self.pages: msg = f'{msg}{self.pages}{"" if self.year else "."} '
+        if self.year: msg = f'{msg}({self.year}). '
+        if self.doi:
+            if self.doi[:4] == 'http':
+                msg = f'{msg}{self.doi}'
+            else:
+                msg = f'{msg}https://doi.org/{self.doi}'
+        return msg
 
 try:
     version = read_version_file(_version_file)
