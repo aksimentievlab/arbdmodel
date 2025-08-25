af8b9a7411348cdf66efa38308f923567657184a
Siddharth Krishnan <sk87@tbgl-work21.physics.illinois.edu>
2025-08-25 12:53:01 -0500

    Merged

--- arbdmodel/calvados3_polymer_model.py
+++ arbdmodel/calvados3_polymer_model.py
@@ -1,136 +1,134 @@
-## Test with `python -m arbdmodel.mpipi_polymer_model`
+## Test with `python -m arbdmodel.calvados3_polymer_model`
 
 import numpy as np
 import sys
 import pandas as pd
 ## Local imports
-from .logger import logger, get_resource_path
-from . import ParticleType, PointParticle
-
+from . import logger, ParticleType, PointParticle, get_resource_path
 from .polymer import PolymerBeads, PolymerModel
 from .interactions import AbstractPotential, HarmonicBond
 
 """Define particle types"""
 _types = dict(
     A = ParticleType("ALA",
-                     mass = 71.08,
+                     mass = 71.07,
                      charge = 0,
                      sigma = 5.04,
-                     freq=0.091,
+                     lambda_=0.3377244362031627
                  ),
     R = ParticleType("ARG",
-                     mass = 156.2,
+                     mass = 156.19,
                      charge = 1,
                      sigma = 6.56,
-                     freq=0.552
+                     lambda_= 0.7407902764839954	
                  ),
     N = ParticleType("ASN",
                      mass = 114.1,
                      charge = 0,
                      sigma = 5.68,
-                     freq=0.353
+                     lambda_= 0.3706962163690402
                  ),
     D = ParticleType("ASP",
-                     mass = 115.1,
+                     mass = 115.09,
                      charge = -1,
                      sigma = 5.58,
-                     freq=0.195,
+                     lambda_=0.092587557536158,
                  ),
     C = ParticleType("CYS",
-                     mass = 103.1,
+                     mass = 103.14,
                      charge = 0,
                      sigma = 5.48,
-                     freq=0.127,
+                     lambda_=0.5922529084601322
                  ),
     Q = ParticleType("GLN",
-                     mass = 128.1,
+                     mass = 128.13,
                      charge = 0,
                      sigma = 6.02,
-                     freq=0.365,
+                     lambda_=0.3143449791669133,
                  ),
     E = ParticleType("GLU",
-                     mass = 129.1,
+                     mass = 129.11,
                      charge = -1,
                      sigma = 5.92,
-                     freq=0.211,
+                     lambda_=0.3706962163690402,
                  ),
     G = ParticleType("GLY",
                      mass = 57.05,
                      charge = 0,
                      sigma = 4.5,
-                     freq=0.22
+                     lambda_=0.7538308115197386
                  ),
     H = ParticleType("HIS",
-                     mass = 137.1,
-                     charge = 0.5,
+                     mass = 137.14,
+                     charge = 0,
                      sigma = 6.08,
-                     freq=0.668
+                     lambda_=0.4087176216525476
                  ),
     I = ParticleType("ILE",
-                     mass = 113.2,
+                     mass = 113.16,
                      charge = 0,
                      sigma = 6.18,
-                     freq=0.005
+                     lambda_=0.5130398874425708
                  ),
     L = ParticleType("LEU",
-                     mass = 113.2,
+                     mass = 113.16,
                      charge = 0,
                      sigma = 6.18,
-                     freq=0.021
+                     lambda_=0.5548615312993875
                  ),
     K = ParticleType("LYS",
-                     mass = 128.2,
+                     mass = 128.17,
                      charge = 1,
                      sigma = 6.36,
-                     freq=0.048
+                     lambda_=0.1380602542039267
                  ),
     M = ParticleType("MET",
                      mass = 131.2,
                      charge = 0,
                      sigma = 6.18,
-                     freq=0.073
+                     lambda_=0.5170874160398543
                  ),
     F = ParticleType("PHE",
-                     mass = 147.2,
+                     mass = 147.18,
                      charge = 0,
                      sigma = 6.36,
-                     freq=0.712
+                     lambda_=0.8906449355499866
                  ),
     P = ParticleType("PRO",
                      mass = 97.12,
                      charge = 0,
                      sigma = 5.56,
-                     freq=0.144
+                     lambda_=0.3469777523519372
                  ),
     S = ParticleType("SER",
                      mass = 87.08,
                      charge = 0,
                      sigma = 5.18,
-                     freq=0.113
+                     lambda_=0.4473142572693176
                  ),
     T = ParticleType("THR",
-                     mass = 101.1,
+                     mass = 101.11,
                      charge = 0,
                      sigma = 5.62,
-                     freq=0.057,
+                     lambda_=0.2672387936544146,
                  ),
     W = ParticleType("TRP",
-                     mass = 186.2,
+                     mass = 186.22,
                      charge = 0,
                      sigma = 6.78,
-                     freq=1.0,
+                     lambda_=1.033450123574512,
                  ),
     Y = ParticleType("TYR",
-                     mass = 163.2,
+                     mass = 163.18,
                      charge = 0,
                      sigma = 6.46,
-                     freq=0.762
+                     lambda_=0.950628687301107
                  ),
     V = ParticleType("VAL",
-                     mass = 99.07,
+                     mass = 99.13,
                      charge = 0,
                      sigma = 5.86,
-                     freq=0.011
+                     lambda_=0.2936174211771383
                  )
 )
 
@@ -141,41 +139,43 @@ for k,t in list(_types.items()):
     ## Add types for IDPs
 
     
-class MpipiNonbonded(AbstractPotential):
-    def __init__(self, debye_length=10, resolution=0.1, range_=(0,None)):
+class CalvadosNonbonded(AbstractPotential):
+    def __init__(self, debye_length=10, resolution=0.1, range_=(0,None),temperature=293.15):
         AbstractPotential.__init__(self, resolution=resolution, range_=range_)
         self.debye_length = debye_length
         self.max_force = 50
-
+        self.temperature = temperature
     def potential(self, r, types):
         """ Electrostatics """
         typeA, typeB = types
         ld = self.debye_length 
-        q1 = typeA.charge*0.75
-        q2 = typeB.charge*0.75
-        D = 80                  # dielectric of water
+        q1 = typeA.charge
+        q2 = typeB.charge
+        T = self.temperature
+        D = 5321/T + 233.76-0.9297*T+1.417*1e-3*T**2 - 8.292*1e-7*T**3                 # dielectric of water
         ## units "e**2 / (4 * pi * epsilon0 AA)" kcal_mol
         A =  332.06371
-        u_elec = (A*q1*q2/D)*np.exp(-r/ld) / r 
-        
-        vij=1
-        muij=2
-
-        if typeA.resname=="ILE":
-            if typeB.resname=="ILE":
-                muij=11
-            elif typeB.resname=="VAL":
-                muij=4
-        if typeB.resname=="ILE" and typeA.resname=="VAL": muij=4
+        rc = 40 ##4nm
+        u_elec = np.zeros_like(r,dtype = float)
+        mask = r<rc
+        u_elec_r = (A*q1*q2/D)*np.exp(-r/ld) / r 
+        u_elec_rc= (A*q1*q2/D)*np.exp(-rc/ld) / rc 
+        u_elec[mask] = u_elec_r[mask] - u_elec_rc
             
-        Rij=3*sigma_ij
-
-        #alpha=2*(3**(2*muij))*((2*vij+1/(2*vij*(3**(2*muij)-1))))**(2*vij+1)
-        alpha=2*vij*(Rij/sigma_ij)**(2*muij)*((2*vij+1)/(2*vij*((Rij/sigma_ij)**(2*muij)-1)))**(2*vij+1)
-        sasa_scaling =1
-        u_wang=sasa_scaling*eps_ij*alpha*((sigma_ij/r)**(2*muij)-1)*((Rij/r)**(2*muij)-1)**(2*vij)
-
-
+        """Ashbaugh-Hatch potential"""
+        sigma = 0.5 * (typeA.sigma + typeB.sigma)
+        lambda_ = 0.5 * (typeA.lambda_ + typeB.lambda_)
+        r6 = (sigma/r)**6
+        r12 = r6**2
+        epsilon = 0.2
+        u_lj = 4 * epsilon * (r12-r6)
+        rc = 20
+        u_lj_rc = 4*epsilon*((sigma/rc)**12 - (sigma/rc)**6)
+        u_AH = np.zeros_like(r,dtype=float)
+        mask1 = r<=2**(1/6.0)*sigma
+        mask2 = (r>2**(1/6.0)*sigma) & (r<=rc)
+        u_AH[mask1] = u_lj[mask1] - lambda_*u_lj_rc + epsilon*(1-lambda_)
+        u_AH[mask2] = lambda_*(u_lj[mask2] - u_lj_rc)
         """ Mpipi scale model """
         """
         A_is_idp = B_is_idp = False
@@ -208,10 +208,10 @@ class MpipiNonbonded(AbstractPotential):
 
         u = u_elec + u_hps
         """
-        u = u_elec + u_wang
+        u = u_elec + u_AH
         return u
 
-class MpipiBeads(PolymerBeads):
+class CalvadosBeads(PolymerBeads):
 
     def __init__(self, polymer, sequence=None,
                  spring_constant = 19.19,
@@ -255,8 +255,8 @@ class MpipiBeads(PolymerBeads):
             pass
 
 
-class MpipiModel(PolymerModel):
-    def __init__(self, polymers,
+class CalvadosModel(PolymerModel):
+    def __init__(self, polymers, 
                  sequences = None,
                  rest_length = 3.8,
                  spring_constant = 19.19,
@@ -270,25 +270,23 @@ class MpipiModel(PolymerModel):
         [damping_coefficient]: ns
         """
 
-        logger.info("""You are using an implementation of the Mpipi Polymer model as described for proteins and RNA based on:
-        Physics-driven coarse-grained model for biomolecular phase separation with near-quantitative accuracy. Published in final edited form as:
-Nat Comput Sci. 2021 Nov; 1(11): 732–743. Published online 2021 Nov 22. doi: 10.1038/s43588-021-00155-3
-
+        logger.info("""You are using an implementation of the Calvados3 Polymer model as described for proteins:
+        A coarse-grained model for disordered and multi-domain proteins. Published in final edited form as:
+Protein Science 33, no. 11 (2024): e5172. Published online 2024 Oct 16. doi:https://doi.org/10.1002/pro.5172
 Please cite all appropriate articles!""")
 
 
         if 'timestep' not in kwargs: kwargs['timestep'] = 10e-6
         if 'cutoff' not in kwargs: kwargs['cutoff'] = max(4*debye_length,20)
-
+        if 'temperature' not in kwargs:  kwargs['temperature'] = 293.15
         if 'decomp_period' not in kwargs:
             kwargs['decomp_period'] = 1000
-
+        self.temperature = kwargs['temperature']
         self.rest_length = rest_length
-        self.spring_constant = spring_constant
-
+        self.spring_constant = spring_constant 
         """ Assign sequences """
         if sequences is None:
-            raise NotImplementedError("MpipiModel must be provided a sequences argument")
+            raise NotImplementedError("Calvados3Model must be provided a sequences argument")
 
         PolymerModel.__init__(self, polymers, sequences, monomers_per_bead_group=1, **kwargs)
 
@@ -298,7 +296,7 @@ Please cite all appropriate articles!""")
         self.set_damping_coefficient( damping_coefficient )
 
         """ Set up nonbonded interactions """
-        nonbonded = MpipiNonbonded(debye_length)
+        nonbonded = CalvadosNonbonded(debye_length,temperature=self.temperature)
         for t in all_types:
             self._add_nonbonded_interaction(nonbonded, t)
                 
@@ -309,7 +307,7 @@ Please cite all appropriate articles!""")
             self.add_nonbonded_interaction( interaction, typeA=type_, typeB=t )
 
     def _generate_polymer_beads(self, polymer, sequence, polymer_index = None):
-        return MpipiBeads(polymer, sequence,
+        return CalvadosBeads(polymer, sequence,
                        rest_length = self.rest_length,
                        spring_constant = self.spring_constant,
                        monomers_per_bead_group = self.monomers_per_bead_group,
